ab30cf7ca65375e2f5859e2eba623a75afaad3dd
shrek.wang <shrekshrek@gmail.com>
2015-02-25 10:36:25 +0800

    update

--- csstween.lite.js
+++ csstween.lite.js
@@ -90,18 +90,140 @@
     });
 
     // --------------------------------------------------------------------主要方法
-    var _requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    function waitHandler(fun){
-        _requestAnimationFrame(function() {
-            _requestAnimationFrame(function() {
-                fun();
-            });
-        });
-    }
-
     var _endEvents = ['transitionend', 'webkitTransitionEnd', 'oTransitionEnd'];
     var _handlers = {};
     var _handlerId = 0;
+
+    var _requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    function _waitHandler(fun){
+        _requestAnimationFrame(function() {
+            fun();
+        });
+    }
+
+    function _getElement(obj){
+        switch(typeof(obj)){
+            case 'string':
+                return (typeof(document) === "undefined") ? obj : (document.querySelectorAll ? document.querySelectorAll(obj) : document.getElementById((obj.charAt(0) === "#") ? obj.substr(1) : obj));
+                break;
+            case 'object':
+                return obj;
+                break;
+            default :
+                throw "dom is undefined, can't tween!!!";
+                break;
+        }
+    }
+
+    function _tween(dom, duration, params){
+        var _dom = dom;
+
+        var _duration = '';
+        if(duration){
+            _duration = duration + 's';
+        }else{
+            _duration = '0s';
+        }
+
+        var _ease = 'cubic-bezier(0, 0, 1, 1)';
+        var _delay = '0s';
+        for(var i in params){
+            switch(i){
+                case "ease":
+                    _ease = 'cubic-bezier' + params[i];
+                    break;
+                case "delay":
+                    _delay = params[i] + 's';
+                    break;
+                case 'onComplete':
+                    for(var j in _endEvents){
+                        _addEventHandler(_dom, _endEvents[j], _endHandler, {dom:_dom, fun:params[i]});
+                    }
+                    break;
+            }
+        }
+
+        _dom.style[_browserPrefix + "Transition"] = 'all ' + _duration + ' ' + _ease + ' ' + _delay;
+
+        _waitHandler(function(){
+            _paramHandler(_dom, params);
+        });
+    }
+
+    function _endHandler(obj){
+        obj.dom.style[_browserPrefix + "Transition"] = 'none';
+        for (var j in _endEvents) {
+            _removeEventHandler(obj.dom, _endEvents[j]);
+        }
+        _waitHandler(function() {
+            obj.fun();
+        });
+    }
+
+    function _paramHandler(dom, params){
+        var _dom = dom;
+        for(var i in params){
+            switch(i){
+                case "ease":
+                case "delay":
+                case 'onComplete':
+                    break;
+                case 'fontWeight':
+                case 'opacity':
+                case 'zoom':
+                    _dom.style[i] = params[i];
+                    break;
+                default:
+                    _dom.style[i] = typeof(params[i]) === 'number' ? params[i] + 'px' : params[i];
+                    break;
+            }
+        }
+    }
+
+    function _addEventHandler(obj, eventName, fun, param){
+        var _self = this;
+        var _fun = fun;
+        if(param)
+        {
+            _fun = function(e)
+            {
+                fun.call(_self, param);
+            }
+        }
+        if(obj.addEventListener){
+            obj.addEventListener(eventName, _fun, false);
+        }else if(obj.attachEvent){
+            obj.attachEvent('on'+eventName, _fun);
+        }else{
+            obj["on" + eventName] = _fun;
+        }
+
+        if(!obj._ct_hid){
+            obj._ct_hid = ++_handlerId;
+        }
+        if(!_handlers[obj._ct_hid]){
+            _handlers[obj._ct_hid] = [];
+        }
+        _handlers[obj._ct_hid].push({event:eventName, handler:_fun, callback:fun});
+    }
+
+    function _removeEventHandler(obj, eventName) {
+        if(!obj._ct_hid) return;
+        var _h = _handlers[obj._ct_hid];
+        for(var i = _h.length - 1; i >= 0; i--){
+            if(_h[i].event === eventName){
+                _fun = _h[i].handler;
+                if (obj.removeEventListener) {
+                    obj.removeEventListener(eventName, _fun);
+                }else if (obj.detachEvent){
+                    obj.detachEvent("on" + eventName, _fun);
+                }else{
+                    delete obj["on" + eventName];
+                }
+                _h.splice(i,1);
+            }
+        }
+    }
 
     CTL.extend({
         from: function(dom, duration, params){
@@ -115,47 +237,36 @@
                 return;
             }
 
-            var _self = this;
-            var _dom = this._getElement(dom);
+            var _dom = _getElement(dom);
 
             if(_dom.length){
                 for(var i = 0, len = _dom.length; i < len; i++){
                     var _params = {};
                     var _d = _dom[i];
                     for(var j in params){
-                        switch(j){
-                            case "ease":
-                            case "delay":
-                            case 'onComplete':
-                                _params[j] = params[j];
-                                break;
-                            default:
-                                _params[j] = _dom[i].style[j];
-                                _d.style[j] = typeof(params[j]) === 'number' ? params[j] + 'px' : params[j];
-                                break;
+                        if(_d.style[j]){
+                            _params[j] = _d.style[j];
+                        }else{
+                            _params[j] = params[j];
                         }
                     }
-                    waitHandler(function() {
-                        _self._tween(_d, duration, _params);
+                    _paramHandler(_d, params);
+                    _waitHandler(function() {
+                        _tween(_d, duration, _params);
                     });
                 }
             }else{
                 var _params = {};
                 for(var j in params){
-                    switch(j){
-                        case "ease":
-                        case "delay":
-                        case 'onComplete':
-                            _params[j] = params[j];
-                            break;
-                        default:
-                            _params[j] = _dom.style[j];
-                            _dom.style[j] = typeof(params[j]) === 'number' ? params[j] + 'px' : params[j];
-                            break;
+                    if(_dom.style[j]){
+                        _params[j] = _dom.style[j];
+                    }else{
+                        _params[j] = params[j];
                     }
                 }
-                waitHandler(function() {
-                    _self._tween(_dom, duration, _params);
+                _paramHandler(_dom, params);
+                _waitHandler(function() {
+                    _tween(_dom, duration, _params);
                 });
             }
         },
@@ -171,14 +282,19 @@
                 return;
             }
 
-            var _dom = this._getElement(dom);
+            var _dom = _getElement(dom);
 
             if(_dom.length){
                 for(var i = 0, len = _dom.length; i < len; i++){
-                    this._tween(_dom[i], duration, params);
+                    var _d = _dom[i];
+                    _waitHandler(function() {
+                        _tween(_d, duration, params);
+                    });
                 }
             }else{
-                this._tween(_dom, duration, params);
+                _waitHandler(function() {
+                    _tween(_dom, duration, params);
+                });
             }
         },
 
@@ -193,41 +309,20 @@
                 return;
             }
 
-            var _self = this;
-            var _dom = this._getElement(dom);
+            var _dom = _getElement(dom);
 
             if(_dom.length){
                 for(var i = 0, len = _dom.length; i < len; i++){
                     var _d = _dom[i];
-                    for(var j in params){
-                        switch(j){
-                            case "ease":
-                            case "delay":
-                            case 'onComplete':
-                                break;
-                            default:
-                                _d.style[j] = typeof(params[j]) === 'number' ? params[j] + 'px' : params[j];
-                                break;
-                        }
-                    }
-                    waitHandler(function(){
-                        _self._tween(_d, duration, params2);
+                    _paramHandler(_d, params);
+                    _waitHandler(function(){
+                        _tween(_d, duration, params2);
                     });
                 }
             }else{
-                for(var j in params){
-                    switch(j){
-                        case "ease":
-                        case "delay":
-                        case 'onComplete':
-                            break;
-                        default:
-                            _dom.style[j] = typeof(params[j]) === 'number' ? params[j] + 'px' : params[j];
-                            break;
-                    }
-                }
-                waitHandler(function(){
-                    _self._tween(_dom, duration, params2);
+                _paramHandler(_dom, params);
+                _waitHandler(function(){
+                    _tween(_dom, duration, params2);
                 });
             }
         },
@@ -243,132 +338,20 @@
                 return;
             }
 
-            var _dom = this._getElement(dom);
+            var _dom = _getElement(dom);
 
             if(_dom.length){
                 for(var i = 0, len = _dom.length; i < len; i++){
                     for(var j in _endEvents){
-                        this._removeEventHandler(_dom[i], _endEvents[j]);
+                        _removeEventHandler(_dom[i], _endEvents[j]);
                     }
                     _dom[i].style[_browserPrefix + "Transition"] = 'none';
                 }
             }else{
                 for(var j in _endEvents){
-                    this._removeEventHandler(_dom, _endEvents[j]);
+                    _removeEventHandler(_dom, _endEvents[j]);
                 }
                 _dom.style[_browserPrefix + "Transition"] = 'none';
-            }
-        },
-
-        _getElement: function(obj){
-            switch(typeof(obj)){
-                case 'string':
-                    return (typeof(document) === "undefined") ? obj : (document.querySelectorAll ? document.querySelectorAll(obj) : document.getElementById((obj.charAt(0) === "#") ? obj.substr(1) : obj));
-                    break;
-                case 'object':
-                    return obj;
-                    break;
-                default :
-                    throw "dom is undefined, can't tween!!!";
-                    break;
-            }
-        },
-
-        _tween:function(dom, duration, params){
-            var _dom = dom;
-
-            var _duration = '';
-            if(duration){
-                _duration = duration + 's';
-            }else{
-                _duration = '0s';
-            }
-
-            var _ease = 'cubic-bezier(0, 0, 1, 1)';
-            var _delay = '0s';
-            for(var i in params){
-                switch(i){
-                    case "ease":
-                        _ease = 'cubic-bezier' + params[i];
-                        break;
-                    case "delay":
-                        _delay = params[i] + 's';
-                        break;
-                    case 'onComplete':
-                        for(var j in _endEvents){
-                            this._addEventHandler(_dom, _endEvents[j], this._endHandler, {dom:_dom, fun:params[i]});
-                        }
-                        break;
-                }
-            }
-
-            _dom.style[_browserPrefix + "Transition"] = 'all ' + _duration + ' ' + _ease + ' ' + _delay;
-
-            waitHandler(function(){
-                for(var i in params){
-                    switch(i){
-                        case "ease":
-                        case "delay":
-                        case 'onComplete':
-                            break;
-                        default:
-                            _dom.style[i] = typeof(params[i]) === 'number' ? params[i] + 'px' : params[i];
-                            break;
-                    }
-                }
-            });
-        },
-
-        _endHandler:function(obj){
-            obj.dom.style[_browserPrefix + "Transition"] = 'none';
-            for(var j in _endEvents){
-                CTL._removeEventHandler(obj.dom, _endEvents[j]);
-            }
-            obj.fun();
-        },
-
-        _addEventHandler:function (obj, eventName, fun, param){
-            var _self = this;
-            var _fun = fun;
-            if(param)
-            {
-                _fun = function(e)
-                {
-                    fun.call(_self, param);
-                }
-            }
-            if(obj.addEventListener){
-                obj.addEventListener(eventName, _fun, false);
-            }else if(obj.attachEvent){
-                obj.attachEvent('on'+eventName, _fun);
-            }else{
-                obj["on" + eventName] = _fun;
-            }
-
-            if(!obj._ct_hid){
-                obj._ct_hid = ++_handlerId;
-            }
-            if(!_handlers[obj._ct_hid]){
-                _handlers[obj._ct_hid] = [];
-            }
-            _handlers[obj._ct_hid].push({event:eventName, handler:_fun, callback:fun});
-        },
-
-        _removeEventHandler:function (obj, eventName) {
-            if(!obj._ct_hid) return;
-            var _h = _handlers[obj._ct_hid];
-            for(var i = _h.length - 1; i >= 0; i--){
-                if(_h[i].event === eventName){
-                    _fun = _h[i].handler;
-                    if (obj.removeEventListener) {
-                        obj.removeEventListener(eventName, _fun);
-                    }else if (obj.detachEvent){
-                        obj.detachEvent("on" + eventName, _fun);
-                    }else{
-                        delete obj["on" + eventName];
-                    }
-                    _h.splice(i,1);
-                }
             }
         }
 
